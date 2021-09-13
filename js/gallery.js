082ee5e155df5d2194ad01d98887527354c82786
Anna-Sergeevna <4042808@gmail.com>
2021-09-13 22:50:37 +0300

    Добавляет разметку и открытие модалки

---
+++ js/gallery.js
@@ -0,0 +1,46 @@
+import galleryItems from './app.js';
+
+const list = document.querySelector('.js-gallery');
+console.log(list);
+const modal = document.querySelector('.js-lightbox');
+console.log(modal);
+const imgModal = document.querySelector('.lightbox__image');
+console.log(imgModal);
+
+
+const imagesList = galleryItems.map(({ preview, original, description } = el) => {
+  return `<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+
+    />
+  </a>
+</li>`
+}).join('');
+
+console.log(imagesList);
+
+list.innerHTML = imagesList;
+
+/ Открытие модалки/
+
+list.addEventListener('click', onModalOpen);
+
+function onModalOpen(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  } else {
+    modal.classList.add('is-open');
+    imgModal.src = e.target.dataset.source;
+  }
+}
+
